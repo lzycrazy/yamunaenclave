eb72c0ff56a301b256b22559d6572c1a795e7fb8
lzycrazy <87261047+lzycrazy@users.noreply.github.com>
2022-01-26 11:57:07 +0530

    Update views.py

--- nature/views.py
+++ nature/views.py
@@ -5,7 +5,7 @@ from django.contrib import messages
 def index(request):
     # allpost=Post.objects.all
     post=Post.objects.all
-    recentpost=Post.objects.order_by('timestamp')[0:4]
+    recentpost=Post.objects.order_by('date')[0:4]
     context={
         'post':post,
         'recentpost':recentpost
