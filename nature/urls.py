62194da6806211c8a3d3b3c21f5e6e48e00e082e
lzycrazy <87261047+lzycrazy@users.noreply.github.com>
2022-01-28 16:57:01 +0530

    added urls

--- nature/urls.py
+++ nature/urls.py
@@ -12,8 +12,11 @@ urlpatterns = [
     path('services/fitness',views.fitness,name='fitness'),
     path('services/park',views.park,name='park'),
     # path('service',views.service,name='service'),
-    path('search',views.search,name='search')
-
+    path('search',views.search,name='search'),
+    path('save_water',views.save_water,name='save_water'),
+    path('solar_energy',views.solar_energy,name='solar_energy'),
+    path('plant_important',views.plant_important,name='plant_important'),
+    path('recycling_helps',views.recycling_helps,name='recycling_helps'),
      
 
 ]
