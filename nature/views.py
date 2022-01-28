fa67411bfec93b51c5f51b6021534538b32b3cb5
lzycrazy <87261047+lzycrazy@users.noreply.github.com>
2022-01-28 16:53:20 +0530

    added views

--- nature/views.py
+++ nature/views.py
@@ -4,15 +4,14 @@ from django.contrib import messages
 
 def index(request):
     query=Post.objects.all
-    post=Post.objects.all
-    
-    recent=Post.objects.order_by('date')[0:4]
+    latest = Post.objects.order_by('date')[0:4]
     context={
         'query':query,
-        'recent':recent,
-        'post':post
+        'latest':latest
     }
     return render(request,'index.html',context)
+
+
 
 def blog(request):
     recentpost2=Post.objects.order_by('date')[0:3]
@@ -34,6 +33,18 @@ def post(request,slug):
     }
     
     return render(request,'post.html',post)
+
+def save_water(request):
+    return render(request,'save_water.html')
+
+def solar_energy(request):
+    return render(request,'solar_energy.html')
+
+def plant_important(request):
+    return render(request,'plant_important.html')
+
+def recycling_helps(request):
+    return render(request,'recycling_helps.html')
 
 def about(request):
     post=Post.objects.all
