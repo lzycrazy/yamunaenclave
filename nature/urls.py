0999258b1fca7411945e981567479e77ffec01e1
lzycrazy <87261047+lzycrazy@users.noreply.github.com>
2022-01-28 17:25:04 +0530

    Update urls.py

--- nature/urls.py
+++ nature/urls.py
@@ -13,10 +13,8 @@ urlpatterns = [
     path('services/park',views.park,name='park'),
     # path('service',views.service,name='service'),
     path('search',views.search,name='search'),
-    path('save_water',views.save_water,name='save_waterr'),
-    path('solar_energy',views.solar_energy,name='solar_energy'),
-    path('plant_important',views.plant_important,name='plant_important'),
-    path('recycling_helps',views.recycling_helps,name='recycling_helps'),
+    path('savewater/',views.save_water,name='savewaterr'),
+  
      
 
 ]
