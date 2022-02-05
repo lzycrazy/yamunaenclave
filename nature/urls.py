3dd33e6c923332fe3a153f56a84be440a50ef9f6
lzycrazy <87261047+lzycrazy@users.noreply.github.com>
2022-02-05 18:52:47 +0530

    Update urls.py

--- nature/urls.py
+++ nature/urls.py
@@ -12,9 +12,7 @@ urlpatterns = [
     path('services/fitness',views.fitness,name='fitness'),
     path('services/park',views.park,name='park'),
     # path('service',views.service,name='service'),
-    path('search',views.search,name='search'),
-    path('water',views.water,name='water'),
-    path('save_water',views.save_water,name='save_water')
+   
    
   
      
