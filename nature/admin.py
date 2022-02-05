70cfaac1a831054a4eb82b6a7c16b4b6af9b7c6a
lzycrazy <87261047+lzycrazy@users.noreply.github.com>
2022-02-05 17:34:36 +0530

    qsqw

--- nature/admin.py
+++ nature/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from . models import Post,Shop
+from . models import Post,Shop,Singup
 
 # Register your models here.
 admin.site.register(Post)
 admin.site.register(Shop)
+admin.site.register(Singup)
